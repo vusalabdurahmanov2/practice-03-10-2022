10cd1ffe7c5b0b40c5f7d0bc547bd39a6a9ec73d
vusalabdurahmanov2 <100927412+vusalabdurahmanov2@users.noreply.github.com>
2022-03-10 15:59:55 +0400

    Add files via upload

---
+++ task-03-10-2022/task-03-10-2022/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace task_03_10_2022
+{
+    class Program
+    {
+        #region. Ədədlərdən ibarət arrayın mənfi elementlərini müsbətə çevirən method yazın
+        //static void Main(string[] args)
+        //{
+        //    int[] arr = { -5, 5, 40, 10, 60, -90 };
+        //    NegativeToPostivie(ref arr);
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        Console.WriteLine(arr[i]);
+        //    }
+        //}
+        //static void NegativeToPostivie(ref int[] arr)
+        //{
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] < 0) arr[i] = arr[i] * -1;
+        //    }
+        //}
+        #endregion
+        #region Ədədlərdən ibarət arrayın ən kiçik və ən böyük elementinin yerini dəyişən method 
+        //static void Main(string[] args)
+        //{
+        //    int[] arr = { 1, 2, 3, 4, 5, -5, 8, 9, 5 };
+        //    swap(arr);
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        Console.Write($"{arr[i]} ");
+        //    }
+
+
+        //}static int swap(int[] arr)
+        //{
+        //    int min_index = 0;
+        //    int max_index = 0;
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[min_index] > arr[i]) min_index = i;
+        //        else if (arr[max_index] < arr[i]) max_index = i;
+        //    }
+        //    arr[min_index] = arr[max_index] + arr[min_index];
+        //    arr[max_index] = arr[min_index] - arr[max_index];
+        //    arr[min_index] = arr[min_index] - arr[max_index];
+        //    return 0;
+        //}
+        #endregion
+    }
+
+
+}
